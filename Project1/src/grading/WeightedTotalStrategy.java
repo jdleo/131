fc458bd6572175a4d32f5a0205277b540b2bd74d
John Leonardo <jdleonardo5@icloud.com>
2018-09-27 10:40:51 -0700

    WeightedTotalStrategy class implemented

--- Project1/src/grading/WeightedTotalStrategy.java
+++ Project1/src/grading/WeightedTotalStrategy.java
@@ -1,5 +1,71 @@
 package grading;
+import java.util.Map;
+import java.util.List;
+import grading.SizeException;
 
-public class WeightedTotalStrategy {
+public class WeightedTotalStrategy implements GradingStrategy {
+	
+	public Map<String, Double> weights;
+	
+	/**
+	 * calculate() method to conform to GradingStrategy type
+	 * @param key
+	 * @param grades
+	 * @return Grade : weighted total of the list of grades
+	 * @throws SizeException 
+	 */
+	public Grade calculate(String key, List<Grade> grades) throws SizeException {
+		if (grades == null) {
+			throw new SizeException("List of grades cannot be null.");
+		} else if (grades.isEmpty()) {
+			throw new SizeException("List of grades cannot be empty.");
+		} else {
+			//to keep track of running totals
+			double weightedTotalMax = 0;
+			double weightedTotalActual = 0;
+			
+			//iterate over grades list
+			for (Grade grade : grades) {
+				double weight = 1.0;
+				
+				//if non-nulls, get the weight
+				if (weights != null && weights.get(grade.getKey()) != null) {
+					weight = weights.get(grade.getKey());
+				}
+				
+				//if weight is less than 0, set to 0
+				if (weight < 0.0) {
+					weight = 0.0;
+				}
+				
+				//handle missing values with Missing utility
+				Missing missing = new Missing();
+				double gradeValue = missing.doubleValue(grade.getValue());
+				
+				//add to running totals
+				weightedTotalMax += (100.0 * weight);
+				weightedTotalActual += (gradeValue * weight);
+			}
+			
+			//calculate final weighted grade
+			double finalGradeValue = 100.0 * (weightedTotalActual / weightedTotalMax);
+			
+			return new Grade(key, finalGradeValue);
+		}
+	}
 
+	/**
+	 * Constructor (default)
+	 */
+	public WeightedTotalStrategy() {
+		this.weights = null;
+	}
+	
+	/**
+	 * Constructor
+	 * @param weights
+	 */
+	public WeightedTotalStrategy(Map<String, Double> weights) {
+		this.weights = weights;
+	}
 }
